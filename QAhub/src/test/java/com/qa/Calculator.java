e8cc867bc5b68adb873877bd556101eccac01fd4
Deekshith <seniortestengineer4@gmail.com>
2022-09-17 15:50:48 +0530

    cucumber project

---
+++ QAhub/src/test/java/com/qa/Calculator.java
@@ -0,0 +1,85 @@
+package com.qa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Calculator {
+	WebDriver  driver;
+	
+	@Given("User open the chrome")
+	public void user_ope_the_chrome() {
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\OneDrive\\My Documents\\New folder\\QAhub\\src\\test\\resources\\chromedriver.exe");  
+		  driver= new ChromeDriver();
+		  driver.manage().window().maximize();
+	}
+	@Given("^User open website$")
+	public void openwebsite() {
+		driver.get("https://mvnrepository.com/artifact/io.cucumber/cucumber-junit/6.11.0");
+	}
+	@When("^when user click on gradel$")
+	public void clickOnGreadle() {
+		driver.findElement(By.xpath("//a[@href=\"#gradle\"]")).click();
+		
+	}
+	
+	@When("^when user click on SBT$")
+	public void clickOnSBT() {
+		driver.findElement(By.xpath("//a[@href=\"#sbt\"]")).click();
+		
+	}
+	@Then("^User should see implemtation$")
+	public void getGradletext() {
+		try {
+			Thread.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String v=driver.findElement(By.id("gradle-a")).getText();
+		System.out.println("Text area value "+v);
+		assertTrue(v.contains("testImplementation group:"));
+		
+	}
+	
+	
+	@Then("^User should see libraryDependencies$")
+	public void getlibraryDependencies() {
+		try {
+			Thread.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String v=driver.findElement(By.id("sbt-a")).getText();
+		System.out.println("Text area value "+v);
+		assertTrue(v.contains("libraryDependencies"));
+		
+	}
+	
+//		public static void main(String[] args) {
+//			
+//			Calculator m1=new Calculator();
+//				
+//				m1.openChrome();
+//				m1.openwebsite();
+//				try {
+//					Thread.sleep(2);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				m1.clickOnGreadle();
+//				m1.getGradletext();
+//				
+//			}
+			
+
+}
---
+++ QAhub/src/test/java/com/qa2/Demo2.java
@@ -0,0 +1,8 @@
+package com.qa2;
+
+import com.qa.Calculator;
+
+public class Demo2 {
+
+
+}
